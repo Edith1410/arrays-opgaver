2f1439c6e214eeaf798fa6b5a78ba54f69b00d17
Edith Obdrup <edob1000@stud.ek.dk>
2026-09-21 14:39:09 +0200

    objekt film

--- script.js
+++ script.js
@@ -13,15 +13,32 @@
 // }
 
 // playliste:
-const tracks = ["dracula", "Thriller", "cardigan", "seven", "dashboard", "orbiter", "dan", "august", "peace", "asleep"];
+// const tracks = ["dracula", "Thriller", "cardigan", "seven", "dashboard", "orbiter", "dan", "august", "peace", "asleep"];
 
-console.log(tracks);
+// console.log(tracks);
 
-const ul = document.querySelector("ul");
+// const ul = document.querySelector("ul");
 
-tracks.forEach(playliste);
+// tracks.forEach(playliste);
 
-function playliste(tracks) {
-  console.log(tracks);
-  ul.innerHTML += `<p>${tracks}</p>`;
-}
+// function playliste(tracks) {
+//   console.log(tracks);
+//   ul.innerHTML += `<p>${tracks}</p>`;
+// }
+
+// objekt øvelse:
+const Movie = {
+  name: "obsession",
+  genre: "horror",
+  length: "1t49m",
+  director: "Curry Barker",
+};
+
+const card = document.querySelector(".card");
+
+card.innerHTML = `
+<h2>${Movie.name}<h2/>
+<h3>${Movie.genre}<h3>
+<p>${Movie.length}<p>
+<p>${Movie.director}<p>
+`;
